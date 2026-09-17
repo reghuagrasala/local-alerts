4bd523a3068da5fa9f919a9629f87bd1ecb50004
reghuagrasala <87054723+reghuagrasala@users.noreply.github.com>
2026-09-17 09:16:14 +0530

    Refresh app cache for location SACHET update

--- sw.js
+++ sw.js
@@ -1,26 +1,6 @@
-const CACHE="local-alerts-v12";
+const CACHE="local-alerts-v13";
 const CORE=["./","./index.html","./style.css","./app.js","./sachet.js","./gps-fix.js","./data.js","./manifest.json","./icon-192.png","./icon-512.png","./share.png","./contacts.html","./public-contacts.js"];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
-});
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-    )).then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",e=>{
-  const u=new URL(e.request.url);
-  if(u.origin!==location.origin || e.request.method!=="GET") return;
-  e.respondWith(
-    fetch(e.request).then(r=>{
-      const copy=r.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-      return r;
-    }).catch(()=>caches.match(e.request).then(c=>c||caches.match("./index.html")))
-  );
-});
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url);if(u.origin!==location.origin||e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r;}).catch(()=>caches.match(e.request).then(c=>c||caches.match("./index.html"))));});
